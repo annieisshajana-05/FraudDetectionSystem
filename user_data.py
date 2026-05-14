83e4f86fd464dbf858b6e611408ff63c9cc932a1
annieisshajana-05 <annieisshajana@gmail.com>
2026-05-14 22:09:57 +0530

    Add files via upload

---
+++ user_data.py
@@ -0,0 +1,14 @@
+# Simulated user database
+
+user_history = {
+    "user1": {
+        "avg_amount": 500,
+        "last_location": "India",
+        "usual_time": "day"
+    },
+    "user2": {
+        "avg_amount": 2000,
+        "last_location": "USA",
+        "usual_time": "night"
+    }
+}
